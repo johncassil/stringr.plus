7e000df976cb9a3ea4e809ed4400e176531692cc
Abi Dawson <ajdawson57@gmail.com>
2020-10-24 16:44:20 -0400

    Add test for str_extract_context

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -55,6 +55,16 @@ test_that("str_extract_between works", {
 
 })
 
+test_that("str_extract_context works", {
+
+    expect_equal(str_extract_context(string = test_string, pattern = "before", window_size = 3),
+                 "re_before_pa")
+
+    expect_equal(str_extract_context(string = test_string, pattern = "string", window_size = 3),
+                 c("ng_string_he","er_string_he"))
+
+})
+
 
 multiple_test_string <- '1kadlkj raid.who .csv'
 
