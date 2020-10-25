5a37dd379d7dcf3eb41abc45aaf484e05bf6d21d
John Paul Cassil <john@johncassil.com>
2020-10-25 12:49:19 -0400

    Added str_highlight_context.R

--- R/str_extract_context.R
+++ R/str_extract_context.R
@@ -31,3 +31,41 @@ str_extract_context <- function(string, pattern, window_size){
 
     return(string_extract_all)
 }
+
+
+
+#' Extract and print pattern from a string along with characters that occur before and after the pattern with helpful highlighting.
+#'
+#' @param string The string from which to extract.
+#' @param pattern Pattern to look for.
+#' @param window_size Number of characters to return before and after the pattern.
+#'
+#' @return A character vector with pattern matches and their context.
+#' @export
+#'
+#' @examples
+#' test_string <- 'To be or not to be, that is the question.'
+#' str_highlight_context(test_string, pattern = 'be', window_size = 4)
+#'
+str_highlight_context <- function(string, pattern, window_size){
+
+    positions_of_pattern <- as.data.frame(stringr::str_locate_all(string=string, pattern = pattern))
+
+    string_extract_all <- rep(NA,nrow(positions_of_pattern))
+
+    for(i in 1:nrow(positions_of_pattern)){
+        start_of_pattern <- positions_of_pattern[i,"start"]
+        end_of_pattern <- positions_of_pattern[i,"end"]
+
+        string_extract <- stringr::str_sub(string = string,
+                                           start = max(0, start_of_pattern - window_size),
+                                           end = min(nchar(string),end_of_pattern + window_size)
+        )
+
+        string_extract_highlighted <- stringr::str_replace_all(string_extract, pattern, glue::glue_col("{cyan {pattern}}"))
+        string_extract_all[i] <- string_extract_highlighted
+
+    }
+
+    return(cat(string_extract_all))
+}
