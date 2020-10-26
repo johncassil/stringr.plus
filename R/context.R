98a0e0c0efa79a2f9fdd55bc9a412d3137a0c967
John Paul Cassil <john@johncassil.com>
2020-10-25 22:32:47 -0400

    Broke apart the two context functions, vectorized them, and wrote tests.

--- R/context.R
+++ R/context.R
@@ -1,5 +1,6 @@
 #' Extract pattern from a string along with characters that occur before and after the pattern.
 #'
+#'#' Vectorised over `string`, `pattern`, and `window_size`.
 #' @param string The string from which to extract.
 #' @param pattern Pattern to look for.
 #' @param window_size Number of characters to return before and after the pattern.
@@ -13,30 +14,80 @@
 #'
 str_extract_context <- function(string, pattern, window_size = 10){
 
+    positions_of_pattern <- stringr::str_locate(string = string, pattern = pattern)
+    string_extract_all <- rep(character(0), nrow(positions_of_pattern))
+
+    window_size <- if(length(window_size) == 1) {
+        rep(window_size, length(positions_of_pattern))
+    } else {
+        window_size
+    }
+
+
+    for(i in 1:nrow(positions_of_pattern)){
+        start_of_pattern <- positions_of_pattern[i,"start"]
+        end_of_pattern <- positions_of_pattern[i,"end"]
+        string_extract <- stringr::str_sub(string = string[i],
+                                           start = max(0,
+                                                       start_of_pattern - window_size[i]),
+                                           end = min(nchar(string[i]),
+                                                     end_of_pattern + window_size[i])
+        )
+
+        string_extract_all[i] <- string_extract
+    }
+
+    return(string_extract_all)
+}
+
+#' Extract all patterns from a string along with characters that occur before and after the patterns.
+#'
+#'#' Vectorised over `string`, `pattern`, and `window_size`.
+#' @param string The string from which to extract.
+#' @param pattern Pattern to look for.
+#' @param window_size Number of characters to return before and after the pattern.
+#'
+#' @return A character vector with pattern matches and their context.
+#' @export
+#'
+#' @examples
+#' test_string <- 'To be or not to be, that is the question.'
+#' str_extract_context_all(test_string, pattern = 'be', window_size = 4)
+#'
+str_extract_context_all <- function(string, pattern, window_size = 10){
+
     positions_of_pattern <- stringr::str_locate_all(string = string, pattern = pattern)
+
+    window_size <- if(length(window_size) == 1) {
+        rep(window_size, length(positions_of_pattern))
+    } else {
+        window_size
+    }
 
     extract_per_string <- function(string, positions_of_pattern, window_size){
         positions_of_pattern <- as.data.frame(positions_of_pattern)
-        string_extract_all <- rep(NA, nrow(positions_of_pattern))
-        for(i in 1:nrow(positions_of_pattern)){
-            start_of_pattern <- positions_of_pattern[i,"start"]
-            end_of_pattern <- positions_of_pattern[i,"end"]
-            string_extract <- stringr::str_sub(string = string,
-                                               start = max(0,
-                                                           start_of_pattern - window_size),
-                                               end = min(nchar(string),
-                                                         end_of_pattern + window_size)
-            )
-
-            string_extract_all[i] <- string_extract
+        string_extract_all <- rep(character(0), nrow(positions_of_pattern))
+        if(nrow(positions_of_pattern) > 0){
+            for(i in 1:nrow(positions_of_pattern)){
+                start_of_pattern <- positions_of_pattern[i,"start"]
+                end_of_pattern <- positions_of_pattern[i,"end"]
+                string_extract <- stringr::str_sub(string = string,
+                                                   start = max(0,
+                                                               start_of_pattern - window_size),
+                                                   end = min(nchar(string),
+                                                             end_of_pattern + window_size)
+                )
+                string_extract_all[i] <- string_extract
+            }
         }
 
-        cleaned_up <- stringr::str_c(string_extract_all, collapse = "...")
-
-        return(cleaned_up)
+        return(string_extract_all)
     }
 
     return_list <- unname(mapply(extract_per_string, string, positions_of_pattern, window_size))
 
     return(return_list)
 }
+
+
+
--- tests/testthat/test-context.R
+++ tests/testthat/test-context.R
@@ -3,11 +3,13 @@ test_string <- 'URL.com/long_string_here_before_pattern_after_another_string_her
 
 test_that("str_extract_context works", {
 
-    expect_equal(str_extract_context(string = test_string, pattern = "before", window_size = 3),
-                 "re_before_pa")
+    expect_equal(
+        str_extract_context(string = test_string, pattern = "before", window_size = 3),
+        "re_before_pa")
 
-    expect_equal(str_extract_context(string = test_string, pattern = "string", window_size = 3),
-                 c("ng_string_he...er_string_he"))
+    expect_equal(
+        str_extract_context(string = test_string, pattern = "string", window_size = 3),
+        "ng_string_he")
 
 })
 
@@ -18,6 +20,8 @@ test_data <- tibble::tribble(
     "This beef is incredible. I like all beef, but Angus beef is the best", "beef", 4,
     "I'm trying to become a vegetarian, though!", "vegetarian", 10)
 
+
+
 test_that("str_extract_context vectorization works", {
     expect_equal(
         test_data %>%
@@ -27,8 +31,61 @@ test_that("str_extract_context vectorization works", {
             ~context,
             "s my first test",
             "is a second one.",
-            "his beef is ...all beef, bu...gus beef is ",
-            " become a vegetarian, though!")
+            "his beef is ",
+            "become a vegetarian, though!")
+    )
+})
+
+
+test_that("str_extract_context vectorization works with static window", {
+    expect_equal(
+        test_data %>%
+            dplyr::mutate(context = str_extract_context(text, keyword, 5)) %>%
+            dplyr::select(context),
+        tibble::tribble(
+            ~context,
+            "s my first test",
+            "is a second one.",
+            "This beef is i",
+            "me a vegetarian, tho")
+    )
+})
+
+
+
+
+test_that("str_extract_context_all vectorization works", {
+    expect_equal(
+        test_data %>%
+            dplyr::mutate(context =
+                              str_extract_context_all(text, keyword, needed_context)) %>%
+            dplyr::select(context),
+
+        tibble::tribble(
+            ~context,
+            's my first test',
+            'is a second one.',
+            c("his beef is ", "all beef, bu", "gus beef is "),
+            ' become a vegetarian, though!'
+        )
+    )
+})
+
+
+test_that("str_extract_context_all vectorization works with static window", {
+    expect_equal(
+        test_data %>%
+            dplyr::mutate(context =
+                              str_extract_context_all(text, keyword, 5)) %>%
+            dplyr::select(context),
+
+        tibble::tribble(
+            ~context,
+            's my first test',
+            'is a second one.',
+            c("This beef is i", " all beef, but", "ngus beef is t"),
+            'me a vegetarian, tho'
+        )
     )
 })
 
